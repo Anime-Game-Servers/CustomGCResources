6fd4d23c1a32d7ad83a87e6bd487a1de29ed93cf
hartie95 <mail@hartie95.de>
2022-12-30 09:06:34 +0100

    Add mapping and script example for the elemental monuments and more (thx @StartForKiller )

---
+++ Scripts/Gadget/IceController.lua
@@ -0,0 +1,12 @@
+function OnBeHurt(context, element_type, unkParam, is_host)
+    ScriptLib.PrintContextLog(context, "Test IceController")
+    local state = ScriptLib.GetGadgetState(context)
+    if element_type == ElementType.Ice then
+        if state == GadgetState.Default or state == GadgetState.GearAction1 then
+            ScriptLib.SetGadgetState(context, GadgetState.GearStart)
+        end
+        if state == GadgetState.Action01 then
+            ScriptLib.SetGadgetState(context, GadgetState.Action02)
+        end
+    end
+end
